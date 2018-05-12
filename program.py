09b41a26e5737d656b72f7a10cd74e7936fa2a1f
Emil Slavov <the_exar@yahoo.com>
2018-05-13 00:09:24 +0300

    removed machine specific settings file

--- program.py
+++ program.py
@@ -1,9 +1,7 @@
 import sys
 
 def main():
-    path = 'D:\\Programming\\python-exercises\\google-python-exercises\\basic\\alice.txt'
-    # print_words(path)
-    # print_top(path)
+    return
 
 if __name__ == '__main__':
     main()
